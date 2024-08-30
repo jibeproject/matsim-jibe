4ab549e6b26457b9d4c5d59411172716b580b50e
MahsaAll <mahsa.abdollahyar@gmail.com>
2024-08-30 18:11:35 +1000

    updated freespeed and capacity of primary and secondary links

--- src/main/java/demand/CreateVehicleNetwork.java
+++ src/main/java/demand/CreateVehicleNetwork.java
@@ -21,8 +21,8 @@ import java.util.List;
 import java.util.Set;
 
 public class CreateVehicleNetwork {
-    private final static double URBAN_NONPRIMARY_CAPACITY_REDUCTION_FACTOR = 0.25;
-    private final static double URBAN_NONPRIMARY_FREESPEED_REDUCTION_FACTOR = 0.25;
+    private final static double CAPACITY_REDUCTION_FACTOR = 0.25;
+    private final static double FREESPEED_REDUCTION_FACTOR = 0.25;
     private final static Logger log = Logger.getLogger(CreateVehicleNetwork.class);
     private static final List<String> PAIRS_TO_CONNECT = List.of("227825out","224795out","164749out","298027out",
             "220563out","128831out","367168out","273137out","124102out","124103out","81480out","8582out","4084out","4083out",
@@ -54,12 +54,18 @@ public class CreateVehicleNetwork {
                 link.setCapacity(2 * link.getCapacity());
             }
 
-            // REDUCE CAPACITY AND FREESPEED OF URBAN NON-PRIMARY LINKS
+            // REDUCE CAPACITY FOR PRIMARY AND SECONDARY LINKS
+            String type = (String) link.getAttributes().getAttribute("type");
+            boolean primary = type != null && type.contains("primary");
+            boolean secondary = type != null && type.contains("secondary");
+            if(primary || secondary) {
+                link.setCapacity((1-CAPACITY_REDUCTION_FACTOR) * link.getCapacity());
+            }
+
+            // REDUCE FREESPEED FOR URBAN LINKS
             boolean urban = (boolean) link.getAttributes().getAttribute("urban");
-            boolean primary = (boolean) link.getAttributes().getAttribute("primary");
-            if(urban && !primary) {
-                link.setCapacity((1-URBAN_NONPRIMARY_CAPACITY_REDUCTION_FACTOR) * link.getCapacity());
-                link.setFreespeed((1-URBAN_NONPRIMARY_FREESPEED_REDUCTION_FACTOR) * link.getFreespeed());
+            if(urban) {
+                link.setFreespeed((1-FREESPEED_REDUCTION_FACTOR) * link.getFreespeed());
             }
         }
 
